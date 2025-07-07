7916b8c4dbc2a2a2c09fddf4a490cf0019ef7b0b
Edan Odongo <edanodongo05@gmail.com>
2025-07-07 15:37:10 +0300

    added vault app

--- passmanager/settings.py
+++ passmanager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vault',  # Custom app for managing passwords
 ]
 
 MIDDLEWARE = [
