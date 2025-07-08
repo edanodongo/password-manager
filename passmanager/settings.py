2ebd884531dde1038bad8fbbb192e3db62fdc405
Edan Odongo <edanodongo05@gmail.com>
2025-07-08 16:23:04 +0300

    update to password reset

--- passmanager/settings.py
+++ passmanager/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'passmanager.wsgi.application'
 
 
+from decouple import config
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -81,8 +83,17 @@ DATABASES = {
 }
 
 # For testing password reset emails in the console
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'edanodongo04@gmail.com'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'Password Vault <edanodongo04@gmail.com>'
+
 
 
 
