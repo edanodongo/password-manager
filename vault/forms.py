d23390699fd53a29fc6ac9d2b5705614401cb0ab
Edan Odongo <edanodongo05@gmail.com>
2025-07-10 12:28:18 +0300

    update to dashboard

--- vault/forms.py
+++ vault/forms.py
@@ -9,3 +9,28 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+
+
+from django import forms
+from .models import Credential
+
+class CredentialForm(forms.ModelForm):
+    password_raw = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}),
+        label="Password"
+    )
+
+    class Meta:
+        model = Credential
+        fields = ['platform_type', 'name', 'username', 'url_or_developer', 'notes']
+
+    def save(self, commit=True, user=None):
+        instance = super().save(commit=False)
+        if user:
+            instance.user = user
+        instance.set_password(self.cleaned_data['password_raw'])
+        if commit:
+            instance.save()
+        return instance
