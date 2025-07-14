5aeed995d2b02ca90cb06b56fa3611050ed83111
Edan Odongo <edanodongo05@gmail.com>
2025-07-14 12:16:14 +0300

    new changes

--- vault/signals.py
+++ vault/signals.py
@@ -2,6 +2,9 @@ from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from .models import LoginRecord
 
+
+# Signal receiver to log user login events
+# This function is triggered whenever a user logs in, logging their IP address and user agent.
 @receiver(user_logged_in)
 def log_login(sender, request, user, **kwargs):
     LoginRecord.objects.create(
